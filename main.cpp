9b0ce39b4d1b6269b9254a1f695bbed48374efd3
NRM10101 <126091408+NRM10101@users.noreply.github.com>
2023-04-06 15:47:17 +0530

    stack_by_LinkedList1

--- main.cpp
+++ main.cpp
@@ -1,60 +1,63 @@
-///////////////////Lab 05//////////////////
 #include <iostream>
-#include <chrono>
+#include<chrono>
 using namespace std;
-
-////////implement stack using array////////////
-class Stack{   //class stack
-    int stack_arr[500];//defualt create a 500 size of stacck
-    int top=-1;//top=-1 indicates th stack is empty
-public:
-    int pop(){
-        if(top==-1){
-            cout<<"Stack underflow";
-            exit(1);
-        }
-        top--;
-        return stack_arr[top+1];
-    }
-    void display(){
-        cout<<"[";
-        for(int i=0;i<=top;i++){
-            cout<<stack_arr[i]<<",";
-        }
-        cout<<"\b]\n";
-    }
-
-public:
-    void push(int data){
-        if(top==500-1) {
-            cout << "Stack overflow";
-            return;
-        }
-        top++;
-        stack_arr[top]=data;
-    }
+///////////////implement stack using linked list/////////
+struct node{
+    int data;
+    struct node *next;
 };
+
+struct node* head =NULL;
+void push(int data){
+    struct node* tempNode=(struct node*)malloc(sizeof(struct node));
+    tempNode->data=data;
+    tempNode->next=NULL;
+    if(head==NULL){
+        head=tempNode;return;
+    }
+    tempNode->next=head;
+    head=tempNode;
+}
+int pop(){
+    if (head==NULL){cout << "Stack underflow";exit(1);}
+    int data=head->data;
+    head=head->next;
+    return data;
+}
+void display(){
+    if(head==NULL){
+        cout<<"Stack is empty"<<endl;
+        exit(1);
+    }
+    node *tempNode=head;
+    cout<<"[";
+    while(tempNode->next!=NULL){
+        cout<<tempNode->data<<",";
+        tempNode=tempNode->next;
+    }
+    cout<<"\b]\n";
+}
 int main() {
     //////////////////////////////////////////
 
-    cout<<"Stacks implement using arrays"<<endl;
-    Stack stack;   //create a stack
+    cout<<"Stacks implement using LinkedList"<<endl;
 
     auto begin =chrono::high_resolution_clock::now();
     for(int i=0;i<10;i++){
-        stack.push(rand()%100); //push 10 items to the satck
+        push(rand()%100); //push 10 items to the stack
     }
-    stack.display();
-    //for(int i=0;i<5;i++){cout<<pop()<<endl;}
+    display();
+//    for(int i=0;i<5;i++){cout<<pop()<<endl;}
     for(int i=0;i<5;i++){ //pop 5 items from stack
-        stack.pop();
+        pop();
     }
-    stack.display();
-    for(int i=0;i<4;i++){stack.push(rand()%100);}  //push 4 items to the stack
-    stack.display();
+    display();
+    for(int i=0;i<4;i++){push(rand()%100);}  //push 4 items to the stack
+    display();
     auto end =chrono::high_resolution_clock::now();
     auto elapsed =chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
     elapsed=elapsed*1000;
     printf("Time taken is: %.6f milliseconds.\n", elapsed.count() * 1e-9);
     return 0;
 }
+
